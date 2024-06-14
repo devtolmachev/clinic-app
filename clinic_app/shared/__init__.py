193b691a3463f10317e768d88adedcfc1917eb97
Daniil Tolmachev <dev.tolmachev@gmail.com>
2024-06-14 12:07:10 +0300

    update shared/__init__.py

--- clinic_app/shared/__init__.py
+++ clinic_app/shared/__init__.py
@@ -3,8 +3,8 @@
 from typing import Final
 
 CSVS: Final = {
-    "tommorow": "src_csvs/tomorrow.csv",
-    "2hours": "src_csvs/2hours.csv",
-    "reviews": "src_csvs/Reviews.csv",
+    "tommorow": "src_csvs/01/tomorrow.csv",
+    "2hours": "src_csvs/01/2hours.csv",
+    "reviews": "src_csvs/01/Reviews.csv",
     "db": "src_csvs/db.csv",
 }
